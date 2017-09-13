4b1c9699a5aa0b6cdc528e82d71449f6b80e1667
TATARINOV DMITRY <tito_alba@mail.ru>
2017-09-13 10:14:13 +0300

    Домашнее задание по заголовкам газет

---
+++ HW1/HW1.py
@@ -0,0 +1,27 @@
+import urllib.request
+import re
+
+def download_page():
+    req = urllib.request.Request('http://www.vecherniyorenburg.ru/')
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('utf-8')
+    return html
+
+def title_list():
+    page=download_page()
+    reg1=re.compile('<div class="news-name"><a href=".+">.+</a></div>')
+    reg2=re.compile('" title=".+"></a> </div>')
+    titles=reg1.findall(page)+reg2.findall(page)
+    clean_titles=[]
+    for el in titles:
+        clean_el=re.sub('<.*?>|[a-z">=]', '', el)
+        clean_titles.append(clean_el)
+    return clean_titles
+
+def file():
+    list = open('list.txt', 'w', encoding='utf-8')
+    for el in title_list():
+        text = list.write(el + '\n')
+
+file()
+print('Done')
