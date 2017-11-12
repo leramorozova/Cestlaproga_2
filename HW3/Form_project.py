e263302e79594fc120fac9652be30b6cf95fc601
TATARINOV DMITRY <tito_alba@mail.ru>
2017-11-12 17:47:53 +0300

    Анкетный проект к 02.05.17

---
+++ HW3/Form_project.py
@@ -0,0 +1,83 @@
+import json
+from flask import Flask
+from flask import url_for, render_template, request, redirect
+import json_file_maker
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/thanks')
+def thanks():
+    if request.args:
+        name = request.args['name']
+        surname = request.args['surname']
+        secname = request.args['secname']
+        birthyear = request.args['birthyear']
+        education = request.args['education']
+        home = request.args['home']
+        lang = request.args['lang']
+        answer1 = request.args['answer1']
+        answer2 = request.args['answer2']
+        data(name, surname, secname, birthyear, education, home, lang, answer1, answer2)
+    return render_template('thanks.html')
+
+obj=[]
+
+def data(name, surname, secname, birthyear, education, home, lang, answer1, answer2):
+    table = open('metadata.csv', 'w', encoding='utf-8')
+    line=name+','+surname+','+secname+','+birthyear+','+education+','+home+','+lang+','+answer1+','+answer2
+    global obj
+    obj.append(line)
+    for el in obj:
+        text = table.write(el+'\n')
+    table.close()
+
+@app.route('/json')
+def json():
+    json_file_maker.file_maker()
+    return render_template('json.txt')
+
+@app.route('/stats')
+def stats():
+    data=json_file_maker.table(json_file_maker.obj_maker())
+    participants=data[0]
+    av_age=data[1]
+    educated=data[2]
+    moscow_livers=data[3]
+    russian=data[4]
+    stress2_nom=data[5]
+    stress2_abl=data[6]
+    return render_template('stats.html', participants=participants, av_age=av_age, educated=educated,
+                           moscow_livers=moscow_livers, russian=russian, stress2_nom=stress2_nom, stress2_abl=stress2_abl)
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/results')
+def results():
+    if request.args:
+        city = request.args['city']
+        data = json_file_maker.pyfile_maker()
+        names = {}
+        cities = []
+        for dict in data:
+            cities.append(dict["home"])
+        if city not in cities:
+            return redirect('/not_found')
+        else:
+            for dict in data:
+                if dict["home"] == city:
+                    name = dict["surname"] + ' ' + dict["name"] + ' ' + dict["secname"]
+                    names[name] = "(возраст: " + str(2017 - int(dict["birthyear"])) + ")"
+            return render_template('results.html', names=names)
+
+@app.route('/not_found')
+def not_found():
+    return render_template ('not_found.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ HW3/json_file_maker.py
@@ -0,0 +1,58 @@
+import json
+
+def obj_maker():
+    mass=[]
+    file = open('/home/lera/Cestlaproga_2/HW3/metadata.csv', 'r', encoding='utf-8')
+    text = file.read()
+    text=text.split('\n')
+    for line in text:
+        if line!='':
+            ank=line.split(',')
+            d = {"name": ank[0], "surname": ank[1], "secname": ank[2], "birthyear": ank[3], "education": ank[4],
+                "home": ank[5], "lang": ank[6], "answer1": ank[7], "answer2": ank[8]}
+            mass.append(d)
+    file.close()
+    j_obj = json.dumps(mass, ensure_ascii=False, sort_keys=True, indent=4)
+    return j_obj
+
+def file_maker():
+    file=open('./templates/json.txt', 'w', encoding='utf-8')
+    text=file.write(obj_maker())
+    file.close()
+    return text
+
+def table(j_obj):
+    statistics=[]
+    data=json.loads(j_obj)
+    participants=len(data)
+    sum_age=0
+    educated = 0
+    moscow_livers=0
+    russian=0
+    stress2_nom=0
+    stress2_abl=0
+    for dict in data:
+        if dict["home"]=="Москва":
+            moscow_livers+=1
+        if dict["lang"]=="русский":
+            russian+=1
+        if dict["education"]=="высшее":
+            educated+=1
+        sum_age+=2017-int(dict["birthyear"])
+        if dict["answer1"]=="stress2":
+            stress2_nom+=1
+        if dict["answer2"]=="stress2":
+            stress2_abl+=1
+    statistics.append(participants)
+    av_age = sum_age // participants
+    statistics.append(av_age)
+    statistics.append(educated)
+    statistics.append(moscow_livers)
+    statistics.append(russian)
+    statistics.append(stress2_nom)
+    statistics.append(stress2_abl)
+    return statistics
+
+def pyfile_maker():
+    data=json.loads(obj_maker())
+    return data
